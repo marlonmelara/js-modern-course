c7e3321f35f51bdd7ea5c19186cc47340eb7d457
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2023-10-29 15:46:40 -0300

    Modify example file for template literals usage in HTML

--- js/02-2-template-literals-html.js
+++ js/02-2-template-literals-html.js
@@ -1,25 +1,23 @@
-let nombre1 = "Audrey";
-let edad = 32;
-let actual = new Date().getFullYear();
+let nombre = "Audrey";
 let imagen = "audrey.png";
 
-// Forma 1 para formar un string
-let resultado1 = "Mi nombre es " + nombre1 + " y mi edad es " + edad + " años";
+// Aquí se está utilizando la propiedad 'innerHTML' del objeto que retorna 'document.querySelector("body")' para establecer el contenido HTML dentro del elemento <body> de la página.
 
-// Forma 2 usando template literals
-let resultado2 = `Mi nombre es ${nombre1} y mi edad es ${edad} años.
-Mi año de nacimiento es ${calcularNacimiento(edad)}`;
+// Se utilizan Template Literals (backticks ` `) para definir una cadena de texto que contenga HTML. Los Template Literals permiten escribir cadenas de texto multilínea y también interpolar expresiones JavaScript dentro de la cadena usando la sintaxis ${...}.
+
+// Dentro del Template Literal, se crean varios elementos HTML, incluyendo un <div> con clase "container", otro <div> con clase "imagen" que contiene un elemento <img> y un elemento <h1> que muestra un mensaje.
+
+// En el atributo 'src' del elemento <img> se interpola la variable 'imagen' para obtener la ruta de la imagen, y en el elemento <span> se interpola la variable 'nombre1' para mostrar el nombre.
+
+// Las interpolaciones dentro del Template Literal permiten insertar valores dinámicos directamente dentro de la cadena HTML, lo que facilita la creación de contenido HTML dinámico en JavaScript.
 
 document.querySelector("body").innerHTML = `<div class="container">
 <div class="imagen">
 <img src="img/${imagen}"
 </div>
 <h1>Mi nombre es
-<span class="name">${nombre1}</span></h1>
+<span class="name">${nombre}</span></h1>
 </div>
 `;
 
-// Definiendo una función que calcula el año de nacimiento basado en la edad proporcionada
-function calcularNacimiento(miEdad) {
-  return actual - miEdad;
-}
+// Este código es un ejemplo de cómo se pueden utilizar los Template Literals para crear contenido HTML dinámico en JavaScript, lo que puede ser especialmente útil en aplicaciones web interactivas y dinámicas.
